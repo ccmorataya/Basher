7d34430acf3ff44e784b853ab63625a7af56000b
Cristian Morataya <ccmorataya@users.noreply.github.com>
2017-04-25 21:51:49 -0600

    change the name of the list file

--- basher.sh
+++ basher.sh
@@ -13,17 +13,17 @@ elif [ $1 = "crear" ]; then
 	if [ ! -f $2 ]; then
 		echo "Creado el archivo '$2'"
 		touch $2
-		touch LISTA
-		echo $2 > LISTA
+		touch XYZ
+		echo $2 >> XYZ
 	else
 		echo "Ya existe un archivo con el nombre: '$2'"
 	fi
 # List all files created in the dir
 elif [ $1 = "listar" ]; then
-	if [ ! -f LISTA ]; then
+	if [ ! -f XYZ ]; then
 		echo "No se ha creado ningun archivo"
 	else
-		cat LISTA
+		cat XYZ
 	fi
 # Show the designed file in screen
 elif [ $1 = "leer" ]; then
