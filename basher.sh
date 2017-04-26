67ed8a1c263e01e386d052a2167b9dd0e736bfbd
Cristian Morataya <cris.morataya@gmail.com>
2017-04-25 21:15:44 -0600

    Add validation to read the content of the file taked as argument

--- basher.sh
+++ basher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Crate a new file taking as first argument the file name
 if [ $1 = "crear" ]; then
-	if [ ! -f $1 ]; then
-		echo "Ya existe un archivo con el nombre: $1"
-	else
+	if [ ! -f $2 ]; then
 		echo "Creado el archivo $2"
 		touch $2
 		touch LISTA
 		echo $2 > LISTA
+	else
+		echo "Ya existe un archivo con el nombre: $2"
 	fi
 # List all files created in the dir
 elif [ $1 = "listar" ]; then
@@ -18,7 +18,7 @@ elif [ $1 = "listar" ]; then
 	fi
 # Show the designed file in screen
 elif [ $1 = "leer" ]; then
-	cat $2
+	echo "$(cat $2)"
 # Show mini man with the default option
 else
 	echo "Parametro incorrecto"
